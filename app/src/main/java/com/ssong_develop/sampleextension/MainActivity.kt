8b5492fb224f037401a151d8bf304bc640d268c0
SSong-develop <thdgnsrl123@naver.com>
2022-12-11 17:29:22 +0900

    animator

--- app/src/main/java/com/ssong_develop/sampleextension/MainActivity.kt
+++ app/src/main/java/com/ssong_develop/sampleextension/MainActivity.kt
@@ -2,6 +2,8 @@ package com.ssong_develop.sampleextension
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ProgressBar
+import com.ssong_develop.sampleextension.animation.ProgressBarAnimation
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
---
+++ app/src/main/java/com/ssong_develop/sampleextension/animation/ProgressBarAnimation.kt
@@ -0,0 +1,17 @@
+package com.ssong_develop.sampleextension.animation
+
+import android.view.animation.Animation
+import android.view.animation.Transformation
+import android.widget.ProgressBar
+
+class ProgressBarAnimation(
+    private val progressBar: ProgressBar,
+    private val from: Float = 0f,
+    private val to: Float
+): Animation() {
+    override fun applyTransformation(interpolatedTime: Float, t: Transformation?) {
+        super.applyTransformation(interpolatedTime, t)
+        val value = from + (to - from) * interpolatedTime
+        progressBar.progress = value.toInt()
+    }
+}
